00bf24e72f570f0a88f5a52d0c9c226d1ee34cee
gabriella-guimaraes <gabigara13@gmail.com>
2025-07-21 21:23:45 -0300

    first commit

---
+++ ChefIA.Api/ChefIA.Api/Controllers/ReceitasController/ReceitasController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChefIA.Api.Controllers.ReceitasController
+{
+    [ApiController]
+    [Route("[receitas]")]
+    public class ReceitasController : Controller
+    {
+        [HttpGet("/cafe-da-manha")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ChefIA.Infra.CrossCutting/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace ChefIA.Infra.CrossCutting
+{
+    public class Class1
+    {
+    }
+}
